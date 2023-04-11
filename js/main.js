5b69d4c6069f4d807c992e9db51db54af54a51b9
Alex Romanchenko <monsters.alex0@gmail.com>
2023-04-11 17:21:39 +0300

    feat: themes colums, footer

--- js/main.js
+++ js/main.js
@@ -108,9 +108,284 @@ const data = [
         name: `"Hot afternoon"`,
         info: "40*30 cm. 2021",
         status: "all"
-    }
+    },
+    {
+        img: "../assets/img/Portraits/img1.jpg",
+        name: `"Portrait of Marina"`,
+        info: "30*40 cm. 2018",
+        status: "portrait"
+    },
+    {
+        img: "../assets/img/Portraits/img2.jpg",
+        name: `"Blondie in a mask"`,
+        info: "40*50 cm. 2018",
+        status: "portrait"
+    },
+    {
+        img: "../assets/img/Portraits/img3.jpg",
+        name: `"Blondie in a mask"`,
+        info: "40*50 cm. 2018",
+        status: "portrait"
+    },
+    {
+        img: "../assets/img/Portraits/img4.jpg",
+        name: `"Brunette with mask"`,
+        info: "40*50 cm. 2018",
+        status: "portrait"
+    },
+    {
+        img: "../assets/img/Portraits/img5.jpg",
+        name: `"Girl in a hat"`,
+        info: "40*50 cm. 2018",
+        status: "portrait"
+    },
+    {
+        img: "../assets/img/Portraits/img6.jpg",
+        name: `"Portrait of man"`,
+        info: "30*40 cm. 2018",
+        status: "portrait"
+    },
+    {
+        img: "../assets/img/Portraits/img7.jpg",
+        name: `"Portrait of a brunette"`,
+        info: "40*40 cm. 2019",
+        status: "portrait"
+    },
+    {
+        img: "../assets/img/land/img1.jpg",
+        name: `"The small waterfall in forest"`,
+        info: "50*40 cm. 2014",
+        status: "land"
+    },
+    {
+        img: "../assets/img/land/img2.jpg",
+        name: `"Colors of autumn"`,
+        info: "70*50 cm. 2010",
+        status: "land"
+    },
+    {
+        img: "../assets/img/land/img3.jpg",
+        name: `"Mountains in autumn"`,
+        info: "50*40 cm. 2012",
+        status: "land"
+    },
+    {
+        img: "../assets/img/land/img4.jpg",
+        name: `"Mountains in winter"`,
+        info: "50*40 cm. 2012",
+        status: "land"
+    },
+    {
+        img: "../assets/img/land/img5.jpg",
+        name: `"Spring in the mountains"`,
+        info: "50*40 cm. 2013",
+        status: "land"
+    },
+    {
+        img: "../assets/img/land/img6.jpg",
+        name: `"Summer in the mountains"`,
+        info: "50*40 cm. 2013",
+        status: "land"
+    },
+    {
+        img: "../assets/img/land/img7.jpg",
+        name: `"Autumn silence"`,
+        info: "50*40 cm. 2011",
+        status: "land"
+    },
+    {
+        img: "../assets/img/land/img8.jpg",
+        name: `"River Neman"`,
+        info: "70*50 cm. 2010",
+        status: "land"
+    },
+    {
+        img: "../assets/img/land/img9.jpg",
+        name: `"Forest pond"`,
+        info: "50*40 cm. 2012",
+        status: "land"
+    },
+    {
+        img: "../assets/img/land/img10.jpg",
+        name: `"Flowers growing near the river"`,
+        info: "50*40 cm. 2013",
+        status: "land"
+    },
+    {
+        img: "../assets/img/land/img11.jpg",
+        name: `"Japanese Garden"`,
+        info: "50*40 cm. 2011",
+        status: "land"
+    },
+    {
+        img: "../assets/img/land/img12.jpg",
+        name: `"Castle in early morning fog"`,
+        info: "50*40 cm. 2012",
+        status: "land"
+    },
+    {
+        img: "../assets/img/land/img13.jpg",
+        name: `"Italian landscape" `,
+        info: "70*50 cm. 2010",
+        status: "land"
+    },
+    {
+        img: "../assets/img/land/img14.jpg",
+        name: `"Summer meadow"`,
+        info: "50*40 cm. 2009",
+        status: "land"
+    },
+    {
+        img: "../assets/img/land/img15.jpg",
+        name: `"Forest river"`,
+        info: "70*50 cm. 2010",
+        status: "land"
+    },
+    {
+        img: "../assets/img/land/img16.jpg",
+        name: `"River banks"`,
+        info: "50*40 cm. 2019",
+        status: "land"
+    },
+    {
+        img: "../assets/img/land/img17.jpg",
+        name: `"Looking from the top of the mountain"`,
+        info: "50*40 cm. 2019",
+        status: "land"
+    },
+    {
+        img: "../assets/img/other/img1.jpg",
+        name: `"Free gallop"`,
+        info: "70*50 cm. 2012",
+        status: "other"
+    },
+    {
+        img: "../assets/img/other/img2.jpg",
+        name: `"Prague. The Charles Bridge"`,
+        info: "70*50 cm. 2019",
+        status: "other"
+    },
+    {
+        img: "../assets/img/other/img3.jpg",
+        name: `"Ikebana on a blue background"`,
+        info: "30*40 cm. 2012",
+        status: "other"
+    },
+    {
+        img: "../assets/img/other/img4.jpg",
+        name: `"Wine glasses on abstract background"`,
+        info: "40*30 cm. 2010",
+        status: "other"
+    },
+    {
+        img: "../assets/img/other/img5.jpg",
+        name: `"Traveling around cities"`,
+        info: "40*50 cm. 2019",
+        status: "other"
+    },
+    {
+        img: "../assets/img/other/img6.jpg",
+        name: `"The Winner"`,
+        info: "40*50 cm. 2019",
+        status: "other"
+    },
+    {
+        img: "../assets/img/flower/img1.jpg",
+        name: `"Red begonia"`,
+        info: "40*50 cm. 2009",
+        status: "flower"
+    },
+    {
+        img: "../assets/img/flower/img2.jpg",
+        name: `"Orchids"`,
+        info: "50*40 cm. 2019",
+        status: "flower"
+    },
+    {
+        img: "../assets/img/flower/img3.jpg",
+        name: `"Flower ikebana on a black background"`,
+        info: "45*40 cm. 2014",
+        status: "flower"
+    },
+    {
+        img: "../assets/img/flower/img4.jpg",
+        name: `"Pions on abstract background"`,
+        info: "50*40 cm. 2014",
+        status: "flower"
+    },
+    {
+        img: "../assets/img/flower/img5.jpg",
+        name: `"Roses on the rojal"`,
+        info: "50*40 cm. 2010",
+        status: "flower"
+    },
+    {
+        img: "../assets/img/flower/img6.jpg",
+        name: `"Beauty in the garden"`,
+        info: "50*40 cm. 2010",
+        status: "flower"
+    },
+    {
+        img: "../assets/img/flower/img7.jpg",
+        name: `"Violet candles"`,
+        info: "30*40 cm. 2012",
+        status: "flower"
+    },
+    {
+        img: "../assets/img/flower/img8.jpg",
+        name: `"Peonies in ray sun"`,
+        info: "50*40 cm. 2011",
+        status: "flower"
+    },
+    {
+        img: "../assets/img/flower/img9.jpg",
+        name: `"Fantasy peony"`,
+        info: "40*50 cm. 2011",
+        status: "flower"
+    },
+    {
+        img: "../assets/img/flower/img10.jpg",
+        name: `"Rose and pitcher"`,
+        info: "40*50 cm. 2009",
+        status: "flower"
+    },
+    {
+        img: "../assets/img/flower/img11.jpg",
+        name: `"Sea Fantasy"`,
+        info: "50*40 cm. 2011",
+        status: "flower"
+    },
+    {
+        img: "../assets/img/flower/img12.jpg",
+        name: `"Tulips in a wooden shoe"`,
+        info: "40*30 cm. 2010",
+        status: "flower"
+    },
+    {
+        img: "../assets/img/flower/img13.jpg",
+        name: `"Bright flower ikebana"`,
+        info: "40*50 cm. 2018",
+        status: "flower"
+    },
+    {
+        img: "../assets/img/flower/img14.jpg",
+        name: `"Flowers in love"`,
+        info: "40*30 cm. 2015",
+        status: "flower"
+    },
+    {
+        img: "../assets/img/flower/img15.jpg",
+        name: `"Ikebana on a colored background"`,
+        info: "40*50 cm. 2010",
+        status: "flower"
+    },
+
 ]
 const list = document.querySelector(".cards")
+const portraitsList = document.querySelector(".portrait")
+const landscapeList = document.querySelector(".land")
+const otherList = document.querySelector(".other")
+const flowerList = document.querySelector(".flower")
 for (let i = 0; i < data.length; i++) {
     if (data[i].status == "all") {
         let card = document.createElement('div')
@@ -122,5 +397,49 @@ for (let i = 0; i < data.length; i++) {
             </div>
         `
         list.append(card);
+    }
+    if (data[i].status == "portrait") {
+        let card = document.createElement('div')
+        card.className = 'card'
+        card.innerHTML = `
+            <div class="card-img"><img src="${data[i].img}" alt="portrait"></div>
+            <div class="card-info">
+                ${data[i].name} <br> ${data[i].info}
+            </div>
+        `
+        portraitsList.append(card);
+    }
+    if (data[i].status == "land") {
+        let card = document.createElement('div')
+        card.className = 'card'
+        card.innerHTML = `
+            <div class="card-img"><img src="${data[i].img}" alt="land"></div>
+            <div class="card-info">
+                ${data[i].name} <br> ${data[i].info}
+            </div>
+        `
+        landscapeList.append(card);
+    }
+    if (data[i].status == "other") {
+        let card = document.createElement('div')
+        card.className = 'card'
+        card.innerHTML = `
+            <div class="card-img"><img src="${data[i].img}" alt="other"></div>
+            <div class="card-info">
+                ${data[i].name} <br> ${data[i].info}
+            </div>
+        `
+        otherList.append(card);
+    }
+    if (data[i].status == "flower") {
+        let card = document.createElement('div')
+        card.className = 'card'
+        card.innerHTML = `
+            <div class="card-img"><img src="${data[i].img}" alt="flower"></div>
+            <div class="card-info">
+                ${data[i].name} <br> ${data[i].info}
+            </div>
+        `
+        flowerList.append(card);
     }
 }
